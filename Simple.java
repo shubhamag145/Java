b81ffb371a54707db1f1e27424f72128a1cf5e85
shubham agarwal <shubham@questtoquest>
2021-07-21 14:13:13 +0000

    f3

--- Simple.java
+++ Simple.java
@@ -1,6 +1,6 @@
 class Simple{
 public static void main(String arg[]){
 
-System.out.println(" Welcome to Cloud Knowledge delhi ");
+System.out.println(" Welcome to Cloud Knowledge delhi , Have a good learning Exp ");
 }
 }
